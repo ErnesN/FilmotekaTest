b5fafe0cf4d6e3ac653276f2f52b415517b28ad7
Ernes Nafieiev <demodekoz@gmail.com>
2022-12-28 00:15:30 +0200

    add

--- src/js/api.js
+++ src/js/api.js
@@ -7,6 +7,8 @@ const SEARCH_URL = `${BASE_URL}/search/movie`;
 const ID_URL = `${BASE_URL}/movie/`;
 const GANRE_LIST_URL = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
 
+// https://api.themoviedb.org/3/movie/76600/videos?api_key=53f2c47317a563cd2628c68ceb6a6673&language=en-US
+
 export class FetchAPI {
   constructor() {
     this.page = 1;
@@ -26,13 +28,34 @@ export class FetchAPI {
     return await axios.get(`${ID_URL}${id}?api_key=${API_KEY}`);
   }
 
-  async getGenre(genreId) {
+  async getTrailer(id) {
+    return await axios.get(`${ID_URL}/${id}/videos?api_key=${API_KEY}`);
+  }
+
+  async fillGenreList() {
     const response = await axios.get(GANRE_LIST_URL);
-    const genre = response.data.genres.filter(({ id }) => {
+    this.genreList = response.data.genres;
+    return this.genreList;
+  }
+
+  getGenreById(genreId) {
+    const genre = this.genreList.filter(({ id }) => {
       if (id === genreId) {
         return true;
       }
     });
+
     return genre[0].name;
   }
+}
+
+const YOUTUBE_URl_BY_ID =
+  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=';
+
+const YOUTUBE_API_KEY = 'AIzaSyB6GdMv8RD8xISEFufgs3lbWAFN0Q-xs-Q';
+
+export async function getTrailerYouTube(key) {
+  // console.log(key);
+  return await axios.get(`${YOUTUBE_URl_BY_ID}${key}&key=${YOUTUBE_API_KEY}`);
+  // return await axios.get(`https://www.youtube.com/watch?v=${key}`);
 }
--- src/js/createMarkupElemetsGallery.js
+++ src/js/createMarkupElemetsGallery.js
@@ -1,9 +1,23 @@
-export function createMarkupElemetsGallery({
-  id,
-  title,
-  poster_path,
-  release_date = [],
-}) {
+export function createMarkupElemetsGallery(
+  { id, title, poster_path, release_date, genre_ids },
+  fetchApi
+) {
+  const genres = genre_ids
+    .map(genreId => {
+      return fetchApi.getGenreById(genreId);
+    })
+    .join(', ');
+
+  const dateYear = new Date(release_date).getFullYear();
+  let images = '';
+
+  if (!poster_path) {
+    images =
+      'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg';
+  } else {
+    images = `https://image.tmdb.org/t/p/w1280${poster_path}`;
+  }
+
   return `<div class="photo-card"
   data-id=${id}
   >
@@ -11,17 +25,17 @@ export function createMarkupElemetsGallery({
     <img
       class="gallery__image"
       loading="lazy"
-      src="https://image.tmdb.org/t/p/w1280${poster_path}"
+      src="${images}"
       alt="${title}"
-
     />
   </a>
+  <button class="gallery-item-trailer">Дивитися трейлер</button>
 
   <div class="info">
     <p class="info-title">${title}</p>
     <div class="info-other">
-      <p class="info-item">Жанр: </b>Та хер знає</p>
-      <p class="info-item">${release_date}</p>
+      <p class="info-item">${genres}</p>
+      <p class="info-item">${dateYear}</p>
     </div>
   </div>
 </div>`;
